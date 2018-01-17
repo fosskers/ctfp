dab4d2427ac5fc48bf889df3c57236c1815ef6a6
Colin Woodbury <colingw@gmail.com>
2018-01-17 10:17:46 -0800

    Product and Coproduct

--- lib/Algebra.hs
+++ lib/Algebra.hs
@@ -4,29 +4,99 @@
 
 module Algebra where
 
-import Data.Ratio
-import GHC.TypeNats
-import Numeric.Natural
-import Prelude hiding (Monoid)
-import Refined
-import Test.QuickCheck
-import Unsafe.Coerce
+import qualified Control.Monad.Trans.State.Lazy as LS
+import qualified Control.Monad.Trans.State.Strict as SS
+import           Data.Functor.Identity (Identity)
+import           Data.Ratio
+import           Numeric.Natural
+import           Prelude hiding (Monoid, id, (.))
+import           Refined
+import           Test.QuickCheck
+import           Unsafe.Coerce
 
 ---
+
+class Category t where
+  -- | An identity morphism.
+  id :: t a a
+
+  -- | Composition of morphisms.
+  (.) :: t b c -> t a b -> t a c
+
+instance Category (->) where
+  id a = a
+
+  (.) f g a = f (g a)
+
+class Category t => Kleisli t where
+  (>=>) :: Monad m => t a (m b) -> t b (m c) -> t a (m c)
+
+-- Is using Monad like this cheating? Or is Kleisli so fundamental that there
+-- couldn't possibly be a complicated implementation.
+--
+-- Should Monad be defined in terms of /this/ instead? Still unclear.
+instance Kleisli (->) where
+  (f >=> g) a = f a >>= g
+
+class Product f where
+  first  :: f a b -> a
+  second :: f a b -> b
+
+instance Product (,) where
+  first  (a, _) = a
+  second (_, b) = b
+
+-- instance Product (LS.State s Identity a) where
+--   first  = LS.execState
+--   second = LS.evalState
+
+class Coproduct f where
+  left  :: a -> f a b
+  right :: b -> f a b
+
+instance Coproduct Either where
+  left  = Left
+  right = Right
 
 -- | Types with a binary, associative operator.
 class Semigroup a where
   (<>) :: a -> a -> a
 
+-- Boring?
+instance Semigroup (a -> a) where
+  (<>) = (.)
+
+instance Semigroup Fraction where
+  Fraction l <> Fraction r = Fraction . unsafeCoerce $ unrefine l * unrefine r
+
+instance Semigroup () where
+  _ <> _ = ()
+
+instance Semigroup Bool where
+  (<>) = (&&)
+
+instance Semigroup Integer where
+  (<>) = (+)
+
 -- | Types with a binary associative operator that also have an "identity" element.
 class Semigroup a => Monoid a where
   identity :: a
 
--- | Types for which each element has an inverse, such that:
--- TODO the law here and elsewhere.
-class Monoid a => Group a where
-  -- | Each element of /a/ must possess an inverse.
-  inverse :: a -> a
+-- Boring?
+instance Monoid (a -> a) where
+  identity = id
+
+instance Monoid Fraction where
+  identity = Fraction $$(refineTH 1)
+
+instance Monoid Integer where
+  identity = 0
+
+instance Monoid () where
+  identity = ()
+
+instance Monoid Bool where
+  identity = False
 
 -- | Non-zero, positive fractional values.
 --
@@ -43,11 +113,11 @@ instance Arbitrary Fraction where
 instance Arbitrary Natural where
   arbitrary = fromIntegral . abs <$> (arbitrary :: Gen Integer)
 
-instance Semigroup Fraction where
-  Fraction l <> Fraction r = Fraction . unsafeCoerce $ unrefine l * unrefine r
-
-instance Monoid Fraction where
-  identity = Fraction $$(refineTH 1)
+-- | Types for which each element has an inverse, such that:
+-- TODO the law here and elsewhere.
+class Monoid a => Group a where
+  -- | Each element of /a/ must possess an inverse.
+  inverse :: a -> a
 
 -- | Positive `Fraction`s under multiplication form a `Group`.
 instance Group Fraction where
@@ -59,29 +129,11 @@ instance Group Fraction where
   -- So, it's safe to use `unsafeCoerce` here.
   inverse = Fraction . unsafeCoerce . recip . unrefine . unfraction
 
-instance Semigroup () where
-  _ <> _ = ()
-
-instance Monoid () where
-  identity = ()
-
 instance Group () where
   inverse _ = ()
 
-instance Semigroup Bool where
-  (<>) = (&&)
-
-instance Monoid Bool where
-  identity = False
-
 instance Group Bool where
   inverse = not
-
-instance Semigroup Integer where
-  (<>) = (+)
-
-instance Monoid Integer where
-  identity = 0
 
 instance Group Integer where
   inverse = negate
--- lib/Category.hs
+++ lib/Category.hs
@@ -1,16 +1,17 @@
 module Category where
 
+import           Algebra
 import           Control.Monad.Trans.State.Strict
 import qualified Data.Map.Strict as M
 import           Prelude hiding (id, (.))
 
 ---
 
-id :: a -> a
-id a = a
+-- id :: a -> a
+-- id a = a
 
-(.) :: (b -> c) -> (a -> b) -> a -> c
-(.) f g a = f (g a)
+-- (.) :: (b -> c) -> (a -> b) -> a -> c
+-- (.) f g a = f (g a)
 
 memoize :: Ord a => (a -> b) -> a -> State (M.Map a b) b
 memoize f a = get >>= maybe g pure . M.lookup a
@@ -29,12 +30,28 @@ badFibs 0 = 0
 badFibs 1 = 1
 badFibs n = badFibs (n-1) + badFibs (n-2)
 
--- >>> go
--- 102334155
--- 102334155
 go :: IO ()
 go = do
   let f = memoize badFibs
       (b, m) = runState (f 40) mempty
   print b
   print $ evalState (f 40) m
+
+------------
+-- CHAPTER 4
+------------
+
+safeRoot :: Double -> Maybe Double
+safeRoot n | n >= 0 = Just $ sqrt n
+           | otherwise = Nothing
+
+safeRecip :: Double -> Maybe Double
+safeRecip 0 = Nothing
+safeRecip n = Just $ 1 / n
+
+-- >>> import Algebra
+-- >>> let f = safeRecip >=> safeRoot
+-- >>> f 4
+-- Just 0.5
+-- >>> f 0
+-- Nothing
